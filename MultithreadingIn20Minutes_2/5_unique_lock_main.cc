73d8f4b97226bfe88229176da78eba7a39b432f0
winr7x <winr7x@gmail.com>
2022-05-26 14:52:02 +0300

    MultithreadingIn20Minutes: Add 5_unique_lock_main

---
+++ MultithreadingIn20Minutes_2/5_unique_lock_main.cc
@@ -0,0 +1,58 @@
+// https://youtu.be/jwJ4Eh_2Umo?t=610 k
+
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <numeric>
+#include <thread>
+#include <vector>
+
+std::mutex g_mutex;
+unsigned long g_counter;
+
+const int  increment_times{100};
+
+void Incrementer() {
+  for (size_t i{}; i < increment_times; ++i) {
+    std::unique_lock<std::mutex> ul(g_mutex);
+    g_counter++;
+    ul.unlock();
+    std::cout << "Doing something non-critical..." << std::endl;
+    ul.lock();
+
+    // Here ul will be destroyed and unlocked
+  }
+}
+
+int main() {
+  int N {2};
+  const int  threads_count{100};
+  std::map<int, int> count;
+
+  for (int i{}; i < N; i++) { // N times print out g_counter
+    g_counter = 0;
+    std::vector<std::thread> threads;
+
+    for (int i{}; i < threads_count; i++) {
+      threads.push_back(std::thread(Incrementer));
+    }
+
+    for (auto &t : threads) {
+      t.join();
+    }
+
+    // g_count is expected increment_times * threads_count here
+
+    std::cout << g_counter << ", ";
+              // << (g_counter == 10000 ? "," : "!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!,")
+              // << ' ';
+    ++count[g_counter];
+  }
+
+  std::cout << std::endl;
+  // Assert that we always got 100*100
+  assert(count[increment_times * threads_count] == N);
+
+  return 0;
+}
