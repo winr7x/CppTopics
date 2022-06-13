7511076548fae35f47a829c8d0056cee2cb01e3a
winr7x <winr7x@gmail.com>
2022-06-13 09:17:29 +0300

    14_condition_variable_simple_example_main: Add comments

--- MultithreadingIn20Minutes_2/14_condition_variable_simple_example_main.cc
+++ MultithreadingIn20Minutes_2/14_condition_variable_simple_example_main.cc
@@ -1,3 +1,9 @@
+// ESSENCE: very simple example of conditional variable
+
+// * producer thread notifies consumer thread (without confirmation of receipt of the notification)
+// * if consumer thread will run much later, then its predicate will return true and consumer will be compileted without any waiting of notify_one()
+// * if consumer thread will run much earlier, then its predicate will return false and consumer will wait for notify_one() and will be completed only after that
+
 #include <cassert>
 #include <chrono>
 #include <condition_variable>
