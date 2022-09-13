85df74b5d7403b71d052b8d1b1c3d4dc69a3ff59
winr7x <winr7x@gmail.com>
2022-09-13 15:51:03 +0300

    Leetcode: TwoSum: 0_brute_force: take improvements from 1_hash_map

--- Leetcode/TwoSum/0_brute_force.cpp
+++ Leetcode/TwoSum/0_brute_force.cpp
@@ -1,7 +1,21 @@
 // https://leetcode.com/problems/two-sum/
 
+// This is brute force solution with complexity O(n^2)
+
+// Usage: ./<binary>                  - default output
+//        ./<binary> true             - verbose output
+
 #include <iostream>
 #include <vector>
+
+bool debug {false};
+
+void logCheckPair(const int i, const int j, const int iNum, const int jNum)
+{
+  if (debug) {
+    std::cout << "Check i,j (" << i << "," << j << ") (" << iNum << "+" << jNum << ")\n";
+  }
+}
 
 class Solution
 {
@@ -44,8 +58,11 @@ public:
 
   std::vector<int> twoSum(const std::vector<int> &nums, int target) {
 
-    for (int i = 0; i < static_cast<int>(nums.size()) - 1; ++i) {
+    const auto beforeLast = static_cast<int>(nums.size()) - 1;            // iterate up to penultimate element !
+
+    for (int i = 0; i < beforeLast; ++i) {
       for (int j = i + 1; j < static_cast<int>(nums.size()); ++j) {
+        logCheckPair(i, j, nums[i], nums[j]);
         if (nums[i] + nums[j] == target) {
           return {i, j};
         }
@@ -62,16 +79,23 @@ void print(const std::string &input, const std::vector<int> &result)
   for (const auto &r : result) {
     std::cout << r << " ";
   }
-  std::cout << "}\n";
+  std::cout << "}\n\n";
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+  const auto debugArg = std::string(argv[1]);
+  const auto wasDebugArgSet = argc > 1 &&  debugArg == "true";
+
+  if (wasDebugArgSet) {
+    debug = true;
+  }
+
   Solution solution;
 
   print("solution.twoSum({2,7,11,15}, 9)", solution.twoSum({2,7,11,15}, 9));
   print("solution.twoSum({3,2,4}, 6)", solution.twoSum({3,2,4}, 6));
   print("solution.twoSum({2 1 3 7 8}, 15)", solution.twoSum({2,1,3,7,8}, 15));
 
-  return 1;
+  return EXIT_SUCCESS;
 }
